f814542cd7abaa9159120cca1b5392c55ea36f65
J7 <j7@j7tech.tech>
2026-01-05 20:29:47 -0500

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Surf")]
+[assembly: AssemblyDescription("The J7 Browser For Anyone To Build From")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("J7")]
+[assembly: AssemblyProduct("Surf")]
+[assembly: AssemblyCopyright("Copyright Joseph Altieri ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("015eae4c-efbd-4cd6-b2c8-abcb04550e11")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
+[assembly: NeutralResourcesLanguage("en")]
